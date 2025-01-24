5a9a892d233f85825ba9939a01c757d2bd3df9f4
Het Gurjar <130950604+Het-477@users.noreply.github.com>
2025-01-24 16:14:04 +0530

    add logic for player & computer input and display it on console
    `getPlayerMove()` takes player input when a move button is clicked, and also plays a round when executed
    
    `getComputerMove()` generates a random move as an AI player

---
+++ ui-script.js
@@ -0,0 +1,48 @@
+const movesArray = ['rock', 'paper', 'scissors'];
+
+let humanMove = '';
+let computerMove = '';
+
+function getPlayerMove() {
+    let movesMenu = document.querySelector("#moves-menu");
+
+    movesMenu.addEventListener("click", (e) => {
+        let move = e.target;
+
+        switch (move.id) {
+            case 'rock':
+                humanMove = 'rock';
+                break;
+
+            case 'paper':
+                humanMove = 'paper';
+                break;
+
+            case 'scissors':
+                humanMove = 'scissors';
+                break;
+
+            default:
+                console.log('Invalid move');
+        }
+        playRound(humanMove, getComputerMove())
+    });
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+function getComputerMove() {
+    computerMove = movesArray[getRandomInt(0, 2)];
+    return computerMove;
+}
+
+function playRound(playerMove, computerMove) {
+    console.log(playerMove)
+    console.log(computerMove)
+}
+
+getPlayerMove()
