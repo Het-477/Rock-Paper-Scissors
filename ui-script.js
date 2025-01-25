1514769f68fa703e9c0cc757bb085270b571b4b7
Het Gurjar <130950604+Het-477@users.noreply.github.com>
2025-01-25 16:40:02 +0530

    feat(round): add logic to play rounds and display scores

--- ui-script.js
+++ ui-script.js
@@ -1,7 +1,16 @@
 const movesArray = ['rock', 'paper', 'scissors'];
 
+const roundResultText = document.querySelector("#round-result");
+const playerScoreText = document.querySelector("#player-score");
+const computerScoreText = document.querySelector("#computer-score");
+
 let humanMove = '';
 let computerMove = '';
+let playerScore = 0;
+let computerScore = 0;
+let roundResult = '';
+
+updateRoundResult()
 
 function getPlayerMoveAndPlayRound() {
     let movesMenu = document.querySelector("#moves-menu");
@@ -25,7 +34,11 @@ function getPlayerMoveAndPlayRound() {
             default:
                 console.log('Invalid move');
         }
+        // plays a single round
         playRound(humanMove, getComputerMove())
+
+        // updates the round's score after each round
+        updateRoundResult();
     });
 }
 
@@ -40,14 +53,58 @@ function getComputerMove() {
     return computerMove;
 }
 
+// for debugging purposes
+function logRoundScore(playerScore, computerScore) {
+    console.log(`Your score: ${playerScore}`)
+    console.log(`AI score: ${computerScore}`)
+}
+
 function playRound(playerMove, computerMove) {
     // getPlayerMoveAndPlayRound()
     console.log(`You choose: ${playerMove}`)
     console.log(`AI choose: ${computerMove}`)
+
+    if (playerMove === computerMove) {
+        roundResult = 'Its a tie'
+        console.log(roundResult);
+        logRoundScore(playerScore, computerScore)
+    }
+    if (isPlayerWinner(playerMove, computerMove)) {
+        roundResult = `You Won!`;
+        console.log(roundResult);
+        playerScore++;
+        logRoundScore(playerScore, computerScore)
+    }
+    if (isComputerWinner(playerMove, computerMove)) {
+        roundResult = 'You Lose. AI won';
+        console.log(roundResult);
+        computerScore++;
+        logRoundScore(playerScore, computerScore)
+    }
 }
 
+function isPlayerWinner(playerMove, computerMove) {
+    if (playerMove === 'rock' && computerMove === 'scissors'
+        || playerMove === 'paper' && computerMove === 'rock'
+        || playerMove === 'scissors' && computerMove === 'paper') {
+        return true
+    }
+}
 
+function isComputerWinner(playerMove, computerMove) {
+    if (computerMove === 'rock' && playerMove === 'scissors'
+        || computerMove === 'paper' && playerMove === 'rock'
+        || computerMove === 'scissors' && playerMove === 'paper') {
+        return true;
+    }
+}
 
-// playRound()
+function updateRoundResult() {
+    roundResultText.textContent = roundResult;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+}
+
 getPlayerMoveAndPlayRound()
+// updateRoundResult()
 
